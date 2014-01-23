336278f14c4cd359596e9f25815de279bb1a027a
Gerhard Petracek <gerhard.petracek@irian.at>
2014-01-23 19:26:37 +0100

    method injection

---
+++ ideafork/src/test/java/at/irian/cdiatwork/ideafork/test/backend/MethodInjectionTest.java
@@ -0,0 +1,40 @@
+package at.irian.cdiatwork.ideafork.test.backend;
+
+import at.irian.cdiatwork.ideafork.backend.api.domain.idea.Idea;
+import at.irian.cdiatwork.ideafork.backend.api.domain.idea.IdeaManager;
+import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(CdiTestRunner.class)
+public class MethodInjectionTest {
+
+    private IdeaManager ideaManager;
+
+    @Inject
+    protected void init(IdeaManager ideaManager) {
+        this.ideaManager = ideaManager;
+    }
+
+    @Test
+    public void ideaCreation() {
+        final String topic = "Learn CDI";
+        final String category = "Education";
+        final String description = "Hello Method-Injection!";
+
+        Idea newIdea = this.ideaManager.createIdeaFor(topic, category);
+        newIdea.setDescription(description);
+
+        Assert.assertEquals(topic, newIdea.getTopic());
+        Assert.assertEquals(category, newIdea.getCategory());
+        Assert.assertEquals(description, newIdea.getDescription());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidIdeaCreation() {
+        this.ideaManager.createIdeaFor(null, null);
+    }
+}
