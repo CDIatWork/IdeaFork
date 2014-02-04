ad10b7ab564a19b6b8fa066bf6dff1a955162a27
Gerhard Petracek <gerhard.petracek@irian.at>
2014-02-04 11:04:31 +0100

    specialized beans

--- ideafork/src/test/java/at/irian/cdiatwork/ideafork/test/backend/TestIdeaSavedObserver.java
+++ ideafork/src/test/java/at/irian/cdiatwork/ideafork/test/backend/TestIdeaSavedObserver.java
@@ -1,15 +1,20 @@
 package at.irian.cdiatwork.ideafork.test.backend;
 
 import at.irian.cdiatwork.ideafork.backend.api.domain.idea.Idea;
+import at.irian.cdiatwork.ideafork.backend.impl.logging.IdeaSavedObserver;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Specializes;
 
+@Specializes
 @RequestScoped
-public class TestIdeaSavedObserver {
+public class TestIdeaSavedObserver extends IdeaSavedObserver {
     private boolean isEventObserved;
 
-    protected void onIdeaSavedEvent(@Observes Idea savedIdea) {
+    @Override
+    public void onIdeaSavedEvent(@Observes Idea savedIdea) {
+        super.onIdeaSavedEvent(savedIdea);
         this.isEventObserved = true;
     }
 
